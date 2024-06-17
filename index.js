a5247fd316b98a32b6457b0a05590aa4cb241694
omdumraliya2356 <omdumraliya1430@gmail.com>
2024-06-17 17:34:48 +0530

    port connection

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser');
 const AdminRoutes = require('./routes/adminRoute');
 const connection = require('./config/db');
 const mysql2 = require("./tables/query");
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/api', AdminRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
